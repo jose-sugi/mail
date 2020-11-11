0578d474528a5da845a3264de4b713ad41684301
jose-sugi <h.s.miruku.1121@gmail.com>
2020-11-11 12:21:58 +0900

    second

--- index.php
+++ index.php
@@ -2,24 +2,31 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>メール自動配信フォーム</title>
+    <title>メール配信システム</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <div class="top">
-      <h1>メール自動配信フォーム</h1>
+    <div class="mail_main">
+            <p class="top">新規メール作成</p>
+            <!-- 入力データをanswer.phpに送信 -->
+            <div>
+                <form  method="POST" action="result.php">
+                    <div class="mail recieve_adress">
+                        <input type="text" name="adra" rows="4" cols="40" placeholder="宛先"></textarea>
+                    </div>
+                    <div class="mail send_adress">
+                        <input type="text" id="adrb" name="adrb"  size="40" value=""  placeholder="送信元" required></label> 
+                    </div>
+                    <div class="mail mailbody">
+                        <input type="textarea" name="tx" rows="4" cols="40" placeholder="本文"></textarea>
+                    </div>
+                    <p>配信指定日：</p>
+                    <input type="text" id="dat" name="dat"  size="30" value="" required></label><br>
+                    <br>
+                    <input type="submit" value="登録">
+                  </form>
+            </div>
+                
     </div>
-    <!-- 入力データをanswer.phpに送信 -->
-    <form  method="POST" action="result.php">
-      <label><h3>宛先アドレス：</h3>
-        <textarea name="adra" rows="4" cols="40"></textarea><br>
-      <label><h3>送信元アドレス：</h3>
-        <input type="text" id="adrb" name="adrb"  size="40" value="" required></label><br>
-      <label><h3>本文：</h3>
-        <textarea name="tx" rows="4" cols="40"></textarea><br>
-      <label><h3>配信指定日：</h3>
-        <input type="text" id="dat" name="dat"  size="30" value="" required></label><br>
-        <br>
-        <input type="submit" value="登録">
-      </form>
-    </body>
+  </body>
 </html>
---
+++ start.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>メール配信システム</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <p class="title">メール配信システム</p> 
+            <a href="index.php" class="btn left">メールを送信</a>
+            <a href="reserve.php" class="btn right">送信予定</a>
+        </div>  
+    </main>	  
+</body>
+</html>
+
+
