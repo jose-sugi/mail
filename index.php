28554c5fab051073ede07879f7b828efae0e11f8
jose-sugi <h.s.miruku.1121@gmail.com>
2020-11-12 12:19:11 +0900

    third

--- index.php
+++ index.php
@@ -12,18 +12,24 @@
             <div>
                 <form  method="POST" action="result.php">
                     <div class="mail recieve_adress">
-                        <input type="text" name="adra" rows="4" cols="40" placeholder="宛先"></textarea>
+                        <input type="text" name="adra" rows="4" cols="40" placeholder="宛先"></input>
                     </div>
                     <div class="mail send_adress">
                         <input type="text" id="adrb" name="adrb"  size="40" value=""  placeholder="送信元" required></label> 
                     </div>
                     <div class="mail mailbody">
-                        <input type="textarea" name="tx" rows="4" cols="40" placeholder="本文"></textarea>
+                        <textarea name="tx" rows="4" cols="40"></textarea>
                     </div>
-                    <p>配信指定日：</p>
-                    <input type="text" id="dat" name="dat"  size="30" value="" required></label><br>
-                    <br>
-                    <input type="submit" value="登録">
+                    <div class="mail_date">
+
+                        <input type="radio" name="specify" value="no" checked><label>日時指定なし</label>
+
+                        <input type="radio" name="specify" value="yes"><label>日時指定あり</label>
+
+                        <input type="datetime-local" id="dat" name="dat"  size="30" value="">
+                        <input type="submit" value="登録">
+                    </div>
+                    
                   </form>
             </div>
                 
--- result.php
+++ result.php
@@ -5,23 +5,34 @@ ini_set('display_errors', '1');
 $adra = $_POST['adra'];
 $adrb = $_POST['adrb'];
 $tx = $_POST['tx'];
+$spe = $_POST['specify']; //日時指定あり→yes ,日時指定なし→no
 $dat = $_POST['dat'];
 
-$dsn='mysql:host=localhost;dbname=mailtest';
-$user='root';
-$password='root';
+if ($spe == "yes") { //日時指定ありならDBに保存
+    $dsn='mysql:host=localhost;dbname=mailtest';
+    $user='root';
+    $password='root';
 
-    try{
-      $db=new PDO($dsn,$user,$password);
-      $db ->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-      $sql= "INSERT INTO mail (adressA,adressB,text,date) VALUES (:adressA,:adressB,:tx,:dat)";
-      $stmt = $db->prepare($sql);
-      $stmt->bindParam(':adressA', $adra, PDO::PARAM_STR);
-      $stmt->bindParam(':adressB', $adrb, PDO::PARAM_STR);
-      $stmt->bindParam(':tx', $tx, PDO::PARAM_STR);
-      $stmt->bindParam(':dat', $dat, PDO::PARAM_STR);
-      $stmt->execute();
-    }catch(PDOException $error){
-      echo "error".$error->getMessage();
-    }
+        try{
+          $db=new PDO($dsn,$user,$password);
+          $db ->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+          $sql= "INSERT INTO mail (adressA,adressB,text,date) VALUES (:adressA,:adressB,:tx,:dat)";
+          $stmt = $db->prepare($sql);
+          $stmt->bindParam(':adressA', $adra, PDO::PARAM_STR);
+          $stmt->bindParam(':adressB', $adrb, PDO::PARAM_STR);
+          $stmt->bindParam(':tx', $tx, PDO::PARAM_STR);
+          $stmt->bindParam(':dat', $dat, PDO::PARAM_STR);
+          $stmt->execute();
+        }catch(PDOException $error){
+          echo "error".$error->getMessage();
+        }
+      } else { //日時指定なしなら直接送信
+          $to = $adra;
+          $subject = "自動送信";
+          $message = $tx;
+          $headers = "From:".$adrb;
+
+          mb_send_mail($to, $subject, $message, $headers);
+      }
+    
 ?>
